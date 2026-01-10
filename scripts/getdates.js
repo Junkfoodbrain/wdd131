adbbe8bc13b2fc6663f043372dd62c24508ba3e3
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-01-10 14:48:22 -0800

    updated many things working with instructor video

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+
+const today = new Date();
+document.querySelector(#currentyear");
+   
+currentyear.innerHTML = 'getfullYear(): <span id="currentyear">${today.getFullYear()}</span>'
+"lastmodified" > document.getElementById("lastModified").innerHTML = document.lastModified;
