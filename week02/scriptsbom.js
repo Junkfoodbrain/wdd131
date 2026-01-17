38b52f2eca76ceb2d803f2faf32134691d9bb6c0
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-01-16 17:01:06 -0800

    created delete button w/tutor... still not working. will work on this tomorrow.

--- week02/scriptsbom.js
+++ week02/scriptsbom.js
@@ -1,10 +1,23 @@
 const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
+const button = document.querySelector('#addchap');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = "❌";
-li.appendChild(deleteButton);
-list.append(li);
+input.value = '';
+
+
+button.addEventListener('click', function () {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = "❌";
+    li.appendChild(deleteButton);
+    list.append(li);
+});
+
+deletebutton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+
+    
+
 
