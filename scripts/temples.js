98b7eec4e1d1241a25f7a76d33fba9ad0e356606
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-01-17 15:56:16 -0800

    .

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,10 @@ today.getFullYear();
 
 document.getElementById("lastmodified").innerHTML = document.lastModified;
 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
