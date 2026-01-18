4720fa9cce1b85f50d91f183602787554f4843e5
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-01-17 17:14:57 -0800

    fixed js code

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,8 +4,8 @@ today.getFullYear();
 document.getElementById("lastmodified").innerHTML = document.lastModified;
 
 
-// this is the ham button - does not work
-const mainnav = document.querySelector('.navigation')
+
+const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {
