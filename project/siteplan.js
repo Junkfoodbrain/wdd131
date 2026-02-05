da1b83d71dbac58d41804e3df5c7839d61375343
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-02-04 21:01:37 -0800

    fixing site document

---
+++ project/siteplan.js
@@ -0,0 +1,12 @@
+const today = new Date();
+today.getFullYear();
+
+document.getElementById("lastmodified").innerHTML = document.lastModified;
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
