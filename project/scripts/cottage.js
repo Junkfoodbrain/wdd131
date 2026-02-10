1dfedd54e227cfc9fd686dd576e988aaad6bf69f
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-02-10 14:50:44 -0800

    hamburger button, divs, aside and  @media response

--- project/scripts/cottage.js
+++ project/scripts/cottage.js
@@ -3,11 +3,16 @@ today.getFullYear();
 
 document.getElementById("lastmodified").innerHTML = document.lastModified;
 
+// Put in hambutton for nav toggle
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
-// Put in hambutton for nav toggle
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
 });
+
+function toggleActive(element) {
+    element.classList.toggle("active");
+}
+
